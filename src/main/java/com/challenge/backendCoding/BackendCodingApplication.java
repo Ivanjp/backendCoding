9a668bd6b214268ce76e702e247a312104a7fdb8
Jorge Ivan <jorgeperez@ciencias.unam.mx>
2021-11-08 19:22:08 -0600

    template

---
+++ src/main/java/com/challenge/backendCoding/BackendCodingApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.backendCoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCodingApplication.class, args);
+	}
+
+}
